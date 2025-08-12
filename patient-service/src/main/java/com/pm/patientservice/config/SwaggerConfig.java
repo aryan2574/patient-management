e69588ede83dd288bd8ec58d3d49492f40c7a561
aryan2574 <ayush.aryan94@yahoo.com>
2025-08-12 16:52:50 +0200

    Setup docker for the application and swagger

---
+++ patient-service/src/main/java/com/pm/patientservice/config/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package com.pm.patientservice.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Patient Management API")
+                        .version("1.0")
+                        .description("API for managing patient information")
+                        .contact(new Contact()
+                                .name("Patient Management Team")
+                                .email("support@patientmanagement.com"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("http://www.apache.org/licenses/LICENSE-2.0")));
+    }
+}
